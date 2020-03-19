38b93c6b28da1d24609faf0a362826cf54c6ff86
PixelRayn <david.kowalk@gmail.com>
2020-03-19 13:40:05 +0100

    Added writer to file

--- src/weatherscraper.py
+++ src/weatherscraper.py
@@ -1,5 +1,17 @@
+from webinterface import get_data
+from pandas import DataFrame
+from datetime import date
+
 def main():
-    pass
+    data = get_data()
+
+    data_frame = DataFrame(data)
+
+    current_date = str(date.today())
+
+    data_frame.to_csv(f"./data/{current_date}.csv", header=["Station", "Wetter", "Temperatur", "Windgeschwindigkeit"], index = False)
+
+
 
 if __name__ == '__main__':
     main()

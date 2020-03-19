0083f424de26558fee4c25dbb3cf292bc6b54516
PixelRayn <david.kowalk@gmail.com>
2020-03-19 15:24:39 +0100

    Changed Naming Convention to allow higher resolution

--- src/weatherscraper.py
+++ src/weatherscraper.py
@@ -1,15 +1,20 @@
 from webinterface import get_data
 from pandas import DataFrame
 from datetime import date
+from datetime import datetime
 
 def main():
     data = get_data()
 
     data_frame = DataFrame(data)
 
-    current_date = str(date.today())
+    time = datetime.now()
 
-    data_frame.to_csv(f"./data/{current_date}.csv", header=["Station", "Wetter", "Temperatur", "Windgeschwindigkeit"], index = False, encoding = 'utf8')
+    current_date = str(date.today())
+    current_hour = str(time.hour)
+    current_minute = str(time.minute)
+
+    data_frame.to_csv(f"./data/{current_date}-{current_hour}-{current_minute}.csv", header=["Station", "Wetter", "Temperatur", "Windgeschwindigkeit"], index = False, encoding = 'utf8')
 
 
 

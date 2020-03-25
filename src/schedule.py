0c84cceb04833e4b672858212b79113dfb8ae77a
PixelRayn <david.kowalk@gmail.com>
2020-03-25 14:10:24 +0100

    Added Schedule File

---
+++ src/schedule.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from time import sleep
+from weatherscraper import main as scrape
+
+def main():
+    print("Scheduling Scan")
+    repeat = True
+    time = datetime.now()
+
+    while(repeat):
+        current_hour = time.hour
+
+        if current_hour == 14:
+            try:
+                scrape()
+                repeat = False
+                print("Done")
+            except:
+                print("Failed")
+        else:
+            sleep(5*60) # Check every five minutes
+
+if __name__ == '__main__':
+    main()

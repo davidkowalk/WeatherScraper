f706b72ec2c8184af7c6ef2a6a986ace7d569a77
PixelRayn <david.kowalk@gmail.com>
2020-05-01 19:32:49 +0200

    Update weatherscraper.py

--- src/weatherscraper.py
+++ src/weatherscraper.py
@@ -2,6 +2,10 @@ from webinterface import get_data
 from pandas import DataFrame
 from datetime import date
 from datetime import datetime
+import json
+
+index_file = "data/files.json"
+month = "may"
 
 def main():
     data = get_data()
@@ -16,9 +20,32 @@ def main():
 
     data_frame.to_csv(f"./data/{current_date}-{current_hour}-{current_minute}.csv", header=["Station", "Wetter", "Temperatur", "Windgeschwindigkeit"], index = False, encoding = 'utf8')
 
-    print(f"Created {current_date}-{current_hour}-{current_minute}.csv")
+    fname = f"{current_date}-{current_hour}-{current_minute}.csv"
+
+    print(f"Created {fname}")
+
+    update_index(month, fname)
+
+def update_index(month: str, name: str):
+    index = read_index()
+
+    if month not in index:
+        index[month] = []
+
+    index[month].append(name)
+
+    write_index(index)
 
 
+def read_index():
+    with open(index_file) as index:
+        txt = index.read()
+
+    return json.loads(txt)
+
+def write_index(index):
+    with open(index_file, "w") as file:
+        file.write(json.dumps(index, indent=4))
 
 if __name__ == '__main__':
     main()
